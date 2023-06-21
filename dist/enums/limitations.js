ae559e738bf45911c8a3cbef63d8540f95e538d6
Chanuth10 <it20074418@my.sliit.lk>
2023-06-21 12:54:08 +0530

    remove dist file

--- dist/enums/limitations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Limitations = void 0;
-var Limitations;
-(function (Limitations) {
-    Limitations["orderLimit"] = "orderLimit";
-})(Limitations = exports.Limitations || (exports.Limitations = {}));
-//# sourceMappingURL=limitations.js.map
--- dist/services/interfaces/IRulesService.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=IRulesService.js.map
